f9fbb429e0fc62f241e0a41ed8fab37786e15f15
Suleiman Abubakar <suleiman.u.abubakar@gmail.com>
2021-01-13 08:22:46 +0300

    telegram more commands

--- cotest/bot.py
+++ cotest/bot.py
@@ -1,26 +1,44 @@
 import telebot
 import requests
 import json
-from telebot.types import ReplyKeyboardMarkup,KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 bot = telebot.TeleBot("1410134344:AAGhcCE9QssZi7VALCxnKQqi7v4J5MRJ0SM")
-
 
 
 def Button(message):
     r = requests.get('http://127.0.0.1:8000/api/button')
     data = json.loads(r.text)
-    key = ReplyKeyboardMarkup(True,False)
+    key = ReplyKeyboardMarkup(True, False)
     text = 'Hello {}'.format(message.from_user.first_name)
     for i in range(len(data['list'])):
         button = KeyboardButton(data['list'][i]['name'])
         key.add(button)
-    bot.send_message(message.from_user.id,text ,reply_markup=key)
+    bot.send_message(message.from_user.id, text)
 
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start', 'help', 'wow'])
 def start(message):
     # text = 'Hello {}'.format(message.from_user.first_name)
     # bot.send_message(message.from_user.id,text)
     Button(message)
+
+
+@bot.message_handler(commands=['letBegin'])
+def sendId(message):
+    text = 'Your Telegram Code is #{}'.format(message.from_user.id)
+    bot.send_message(message.from_user.id, text)
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    receivedMsg = message.text
+    if receivedMsg == 'Hello':
+        replyText = 'Hello {}'.format(message.from_user.first_name)
+    else:
+        replyText = 'We ulisikia wp !'
+
+    bot.reply_to(message,replyText)
+
 
 @bot.message_handler(content_types='text')
 def Send_Message(message):
@@ -28,4 +46,3 @@ def Send_Message(message):
 
 
 bot.polling()
-
--- cotest/pdfs/views.py
+++ cotest/pdfs/views.py
@@ -7,18 +7,18 @@ from django.shortcuts import render
 
 
 def genpdf(request):
-    render(request,'ticket.html')
-#     html_string = render_to_string('ticket.html')
-#     html = HTML(string=html_string)
-#     result = html.write_pdf()
+    return render(request,'ticket.html')
+    # html_string = render_to_string('ticket.html')
+    # html = HTML(string=html_string)
+    # result = html.write_pdf()
 
-#     response = HttpResponse(content_type='application/pdf;')
-#     response['Content-Disposition'] = 'inline; filename=list_people.pdf'
-#     response['Content-Transfer-Encoding'] = 'binary'
-#     with tempfile.NamedTemporaryFile(delete=True) as output:
-#         output.write(result)
-#         output.flush()
-#         output = open(output.name, 'r')
-#         response.write(output.read())
+    # response = HttpResponse(content_type='application/pdf;')
+    # response['Content-Disposition'] = 'inline; filename=list_people.pdf'
+    # response['Content-Transfer-Encoding'] = 'binary'
+    # with tempfile.NamedTemporaryFile(delete=True) as output:
+    #     output.write(result)
+    #     output.flush()
+    #     output = open(output.name, 'r')
+    #     response.write(output.read())
 
-#     return response
+    # return response
