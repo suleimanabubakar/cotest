34b98a7ed2522622c7255b837f303e3fd45f18de
suleimanabubakar <suleiman.u.abubakar@gmail.com>
2021-04-03 16:25:55 +0300

    print updated

--- cotest/pdfs/views.py
+++ cotest/pdfs/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.template.loader import render_to_string
-# from weasyprint import HTML
-# import tempfile
+from django.http import HttpResponse,JsonResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
+import tempfile
 # Create your views here.
+import os
+
+dirname = os.path.dirname('D:/')
+
+# def genpdf(request):
+#     # return render(request,'ticket.html')
+#     html_string = render_to_string('ticket.html')
+#     html = HTML(string=html_string)
+#     result = html.write_pdf()
+
+#     response = HttpResponse(content_type='application/pdf;')
+#     response['Content-Disposition'] = 'inline; filename=list_people.pdf'
+#     response['Content-Transfer-Encoding'] = 'binary'
+#     with tempfile.NamedTemporaryFile(delete=True) as output:
+#         output.write(result)
+#         output.flush()
+#         output = open(output.name, 'r')
+#         response.write(output.read())
+
+#     return response
 
 
 def genpdf(request):
-    return render(request,'ticket.html')
-    # html_string = render_to_string('ticket.html')
-    # html = HTML(string=html_string)
-    # result = html.write_pdf()
+    # Make a PDF straight from HTML in a string.
+    html = render_to_string('ticket.html')
 
-    # response = HttpResponse(content_type='application/pdf;')
-    # response['Content-Disposition'] = 'inline; filename=list_people.pdf'
-    # response['Content-Transfer-Encoding'] = 'binary'
-    # with tempfile.NamedTemporaryFile(delete=True) as output:
-    #     output.write(result)
-    #     output.flush()
-    #     output = open(output.name, 'r')
-    #     response.write(output.read())
+    pdf = HTML(string=html).write_pdf()
 
-    # return response
+    if os.path.exists(dirname):
+
+        f = open(os.path.join(dirname, 'mypdf.pdf'), 'wb')
+        f.write(pdf) 
+        
+        # f.open(pdf)
+        os.startfile(dirname+'d.pdf',"print")
+        
+        
+
+
+    return JsonResponse({'msg':'Success'})
